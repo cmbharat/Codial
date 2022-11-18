0265755a2d5167cd44fae66dd5532d7481bc2433
cmbharat <bharatcm70@gmail.com>
2022-11-18 15:14:53 +0530

    Added controller and Router for home

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const port = 8000;
 
 const app = express();
 
+// use express router
+app.use("/", require("./routes/index"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in server ${err}`);
+    return;
   }
+  console.log("server started");
 });
