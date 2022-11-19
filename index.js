c339854830336719c4d03751f5fdd751ab5f324f
cmbharat <bharatcm70@gmail.com>
2022-11-19 23:28:21 +0530

    Added view for home

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 // use express router
 app.use("/", require("./routes/index"));
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in server ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,5 @@ const homeController = require("../controllers/home_controller");
 console.log("router loaded");
 
 router.get("/", homeController.home);
-
+router.use("/users", require("./profile"));
 module.exports = router;
