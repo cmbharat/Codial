9b271c730899bbf390d439721afcfd7cf4a902ec
cmbharat <bharatcm70@gmail.com>
2022-11-23 16:19:48 +0530

    added layout and partials

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function (req, res) {
-  res.end("<h1>profile module</h1>");
+  // res.end("<h1>profile module</h1>");
+  return res.render("profile", {
+    title: "Profile",
+  });
 };
