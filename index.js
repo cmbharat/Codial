ab6623a02b28a9ffc3a2c997d3076c724608f91c
cmbharat <bharatcm70@gmail.com>
2023-02-19 20:00:45 +0530

    added chatting function using socket.io

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const MongoStore = require("connect-mongo");
 const sassMiddleware = require("node-sass-middleware");
 const flash = require("connect-flash");
 const customMiddleware = require("./config/middleware");
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
 app.use(
   sassMiddleware({
     src: "./static/scss",
